257bcc35c7faaacc98269052ac03879d98670964
sean dunaway <sean.dunaway@CHS-15089.attlocal.net>
2022-02-12 21:03:33 -0500

    cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const auth = require("./middleware/auth");
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const mongoSecret = process.env.MONGO_URI;
 
